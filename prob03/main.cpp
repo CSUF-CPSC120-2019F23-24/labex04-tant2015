17daaa895e35441a874287307a9528988c926d7f
hnguyen1186 <hnguyen1186@cs104u-4a.acad.fullerton.edu>
2019-09-14 14:25:10 -0700

    done with prob03

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // Sales prediction
+#include<iostream>
+
+int main()
+{
+  // defind the variable
+
+  const double percent_increase = 0.18;
+
+  const int total_sale = 2103419277;
+
+  unsigned sale_perdiction;
+
+  // calcuation
+
+  sale_perdiction = total_sale * percent_increase + total_sale ;
+
+  // display the result
+
+std::cout << "Last year's sales were $" << total_sale << std::endl;
+std::cout << "This year's sales perdiction: $" << sale_perdiction << std::endl;
+
+
+}
